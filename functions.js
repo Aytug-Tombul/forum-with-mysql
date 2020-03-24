e618b5210046137d9447baea150f8be7d0cfb548
AytugTombul <aytugtombul@gmail.com>
2020-03-24 23:10:55 +0300

    few changes

--- functions.js
+++ functions.js
@@ -1,31 +1,31 @@
+var loggedUsername = "";
 
 $(document).on("click", "#loginBtn", function() {
-    $("#forum").empty();
-    let div= loginDiv();    
-    $("#forum").append(div);
-  });
-
+  $("#forum").empty();
+  let div = loginDiv();
+  $("#forum").append(div);
+});
 
 $(document).on("click", "#registerBtn", function() {
-    $("#forum").empty();
-    let div= registerDiv();    
-    $("#forum").append(div);
-  });
+  $("#forum").empty();
+  let div = registerDiv();
+  $("#forum").append(div);
+});
 
 function forgot() {
   $("#forum").empty();
-    let div= forgotDiv();    
-    $("#forum").append(div);
+  let div = forgotDiv();
+  $("#forum").append(div);
 }
 function backHome() {
   $("#forum").empty();
-    let div= homeDiv();    
-    $("#forum").append(div);
+  let div = homeDiv();
+  $("#forum").append(div);
 }
 function goHealth() {
   $("#forum").empty();
-    let div3= healthDiv();    
-    $("#forum").append(div3);
+  let div3 = healthDiv();
+  $("#forum").append(div3);
 }
 
 $(document).on("click", "#login", function() {
@@ -35,27 +35,25 @@ $(document).on("click", "#login", function() {
     url: "functions.php",
     type: "POST",
     dataType: "text",
-    data: { functionName:"login" , username: loginUserName, password: loginUserPw },
+    data: {
+      functionName: "login",
+      username: loginUserName,
+      password: loginUserPw
+    },
     success: function(res) {
-      res = JSON.parse(res);
       if (res.result == "success") {
         window.alert("Login Success Welcome " + res.username);
         loggedUsername = res.username;
-        $("#loginStatus").remove();
-        $("body").append(blogDiv);
-        listPosts();
-        var panel = res.panelBtn;
-        if (res.panelBtn != null) {
-          $("#addHere").append(String(panel));
-        }
+        $("#addHere").append(
+          `<span class="navbar-text">` + loggedUsername + `</span>`
+        );
+        backHome();
       } else {
         window.alert("Login failed Please Check your username and password");
       }
     }
   });
 });
-
-
 
 $(document).on("click", "#signup", function() {
   var images = $("#image")[0].files[0];
@@ -65,7 +63,7 @@ $(document).on("click", "#signup", function() {
   var referrerVal = $("#referrer").val();
 
   var fd = new FormData();
-  fd.append("functionName","register")
+  fd.append("functionName", "register");
   fd.append("username", usernameVal);
   fd.append("password", passwordVal);
   fd.append("email", emailVal);
@@ -79,7 +77,8 @@ $(document).on("click", "#signup", function() {
     processData: false,
     contentType: false,
     success: function(data) {
-      console.log(data)
+      window.alert(data);
+      backHome();
     }
   });
 });
--- functions.php
+++ functions.php
@@ -9,18 +9,63 @@ require_once "PHPMailer/Exception.php";
 
 
 
+function generetaNewString($len = 10)
+{
+
+    $token = "qpalzmnxskwoeidjcnbvhfurgyt5876439021";
+    $token = str_shuffle($token);
+    $token = substr($token, 0, $len);
+
+    return $token;
+}
+
+
+
+
 $servername = "localhost";
 $DBusername = "root";
 $DBpassword = "";
 $DBname = "forum";
 $pdo = new PDO("mysql:host=$servername;dbname=$DBname", $DBusername, $DBpassword);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-switch($_POST['functionName']) {
+switch ($_POST['functionName']) {
     case 'login':
-       echo "you did it!";
-    break;
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $sql = "SELECT role FROM users WHERE username =? AND password = ?";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(array($username,$password));
+        $r = $stmt->fetch();
+        $role = $r['role'];
+        if ($stmt->rowCount() > 0) {
+
+
+            if ($role == "admin") {
+                $res = array(
+                    "result" => "success",
+                    "username" => $username,
+                    "panelBtn" => '4443'
+                );
+            } else {
+                $res = array(
+                    "result" => "success",
+                    "username" => $username,
+                    "panelBtn" => "1234"
+                );
+            }
+
+            echo json_encode($res);
+        } else {
+            $res = array(
+                "result" => "failed",
+                "username" => $username
+            );
+            echo json_encode($res);
+        }
+        break;
+
     case 'register':
-        
+
         $image = $_FILES['image']['name'];
         $username = $_POST['username'];
         $password = $_POST['password'];
@@ -30,15 +75,20 @@ switch($_POST['functionName']) {
         $target = "images/" . basename($image);
 
         $usernamecheck = $pdo->prepare("SELECT `username` FROM `users` WHERE username=? OR email=?");
-        $usernamecheck->execute(array($username , $email));
-        if (isset($taken) ) {
+        $usernamecheck->execute(array($username, $email));
+        while ($row = $usernamecheck->fetch(pdo::FETCH_ASSOC)) {
+            $taken[] = $row;
+        }
+
+
+        if (isset($taken)) {
             echo $username . " or " . $email . " is already taken";
         } else {
             try {
 
                 $sql = "INSERT INTO `users` (`id`, `username`, `password`, `email`, `referrer`, `photo`, `token`, `token_Expire`,role) VALUES (?,?,?,?,?,?,?,?,?)";
-                $register=$pdo->prepare($sql);
-                $register->execute(array(NULL,$username, $password,$email,$referrer,$image,NULL,date("Y-m-d H:i:s"),"user"));
+                $register = $pdo->prepare($sql);
+                $register->execute(array(NULL, $username, $password, $email, $referrer, $image, NULL, date("Y-m-d H:i:s"), "user"));
                 echo "New record created successfully";
                 if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
                     $msg = "Image uploaded successfully";
@@ -55,16 +105,16 @@ switch($_POST['functionName']) {
                 $mail->SMTPSecure = "ssl";
                 $subject = "Thanks For Registration";
                 $body = "Thanks for registration I hope you can do this :)";
-        
-        
-        
+
+
+
                 $mail->isHTML(true);
                 $mail->setFrom($email, $username);
                 $mail->addAddress($email);
                 $mail->Subject = $subject;
                 $mail->Body = $body;
-        
-        
+
+
                 if ($mail->send()) {
                     echo "   Email is Sent";
                 } else {
@@ -74,7 +124,52 @@ switch($_POST['functionName']) {
                 echo $sql . "<br>" . $e->getMessage();
             }
         }
-        
+        break;
+    case 'forgot':
+        $email = $_POST["email"];
+        $emailcheck = $pdo->query("SELECT `email` FROM `users_tbl` WHERE email='$email'");
+        if ($emailcheck->rowCount() > 0) {
+            try {
+                $token = generetaNewString();
 
+                $sql = "UPDATE users_tbl SET token=?,token_Expire=? WHERE email =?";
+                $forgot =$pdo->prepare($sql);
+                $forgot->execute(array($token,date("Y-m-d H:i:s")));
 
+                $mail = new PHPMailer();
+                $mail->isSMTP();
+                $mail->Host = "smtp.gmail.com";
+                $mail->SMTPAuth = true;
+                $mail->Username = "exampleblogServ@gmail.com";
+                $mail->Password = "blogservice";
+                $mail->Port = 465;
+                $mail->SMTPSecure = "ssl";
+                $subject = "Reset Password";
+                $body = "
+            Hi ,<br><br>
+            
+            In order to reset password, please click on the link below<br>
+            <a href='
+            http://localhost/forum/forum-with-mysql/functions.php?functionName=resetPass&email=$email&token=$token
+            '>http://localhost/forum/forum-with-mysql/functions.php?functionName=resetPass&email=$email&token=$token</a><br><br>
+            Kind Regards,<br>
+            Aytug T.
+            ";
+
+                $mail->isHTML(true);
+                $mail->setFrom($email);
+                $mail->addAddress($email);
+                $mail->Subject = $subject;
+                $mail->Body = $body;
+                if ($mail->send()) {
+                    echo 'Email Found. Please Check your mailbox';
+                } else {
+                    echo "  " . $mail->ErrorInfo;
+                }
+            } catch (PDOException $e) {
+                echo $sql . "<br>" . $e->getMessage();
+            }
+        } else {
+            echo 'Email not found. Please check your inputs.';
+        }
 }
